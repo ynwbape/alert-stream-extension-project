98df1c9451e47ca80dcb65116804dc30f339a02b
Pierre Nangi <71530777+ynwbape@users.noreply.github.com>
2022-10-20 22:25:01 +0200

    Petit MaJ
    Demain on attaquera le son !

--- popup.js
+++ popup.js
@@ -11,6 +11,13 @@ const headers = {
 const info = document.getElementById('streamerStatus');
 const title = document.getElementById('titleStream');
 const views = document.getElementById('twitch-viewer-count');
+const myAudioElement = new Audio('/sound/Dragon Ball Z  Huge Ki Blast Sound Effect.mp3');
+const twitch_toggle = document.getElementById('twitch-sound-alert');
+const youtube_toggle = document.getElementById('youtube-sound-alert');
+
+twitch_toggle.addEventListener('click', toggle => {
+    console.log(toggle, "Hello Button");
+})
 
 function fetchTwitchAPI(url, headers, callback) {
     fetch(url, {
@@ -19,12 +26,16 @@ function fetchTwitchAPI(url, headers, callback) {
     .then(async response => callback(await response.json()))
 }
 
-let color = document.querySelector('.colorStatus');
-
 const callback = function(result) {
     info.innerText = result.data.length ? "Amaazz est entrain de streamer !" : "Hors Ligne !";
     title.innerText = result.data[0].title; 
     views.innerText = result.data[0].viewer_count;
+    if (result == true) {
+        for (let i; i < 1; i++) {
+            myAudioElement.play();
+        }
+    }
 }
 
 fetchTwitchAPI(url, headers, callback);
+
